461ad15b10c38ad413e2c98b01ced4456d10071d
Getlost01 <aagamjain573@gmail.com>
2022-12-11 21:31:49 +0530

    troubleshoot 4

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,7 +1,5 @@
-import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { add } from '../../store/cartSlice';
-import { fetchProducts } from '../../store/productSlice';
 import { STATUSES } from '../../store/productSlice';
 
 const Products = () => {
@@ -9,16 +7,7 @@ const Products = () => {
     const { data: products, status } = useSelector((state) => state.product);
     // const [products, setProducts] = useState([]);
 
-    // useEffect(() => {
-    //     dispatch(fetchProducts());
-    //     // const fetchProducts = async () => {
-    //     //     const res = await fetch('https://fakestoreapi.com/products');
-    //     //     const data = await res.json();
-    //     //     console.log(data);
-    //     //     setProducts(data);
-    //     // };
-    //     // fetchProducts();
-    // }, []);
+
 
     const handleAdd = (product) => {
         dispatch(add(product));
